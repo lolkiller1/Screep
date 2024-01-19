fa9d5636d6bf645f35db1752b73f39bdb10e3604
Joe <lolkiller1@hotmail.com>
2024-01-19 16:16:24 -0500

    autocommit

--- main.js
+++ main.js
@@ -23,8 +23,14 @@ for(const i in Memory.creeps) {
 		name.run(Game.creeps[i])
 	}
 	
+	if (Game.creeps[i].name == "name2") {
+		name.run(Game.creeps[i])
+	}
+	
 }
 
+util.spawnCreep("name", {m:2,w:1,c:1}, {status:"harvesting"}, 'E5N57')
+util.spawnCreep("name2", {m:2,w:1,c:1}, {status:"harvesting"}, 'E5N57')
 util.spawnCreep("tess", {m:2,w:1,c:1}, {status:"harvesting"}, 'E5N57')
 util.spawnCreep("tess2", {m:2,w:1,c:1}, {status:"harvesting"}, 'E5N57')
 util.spawnCreep("tess3", {m:2,w:1,c:1}, {status:"harvesting"}, 'E5N57')
