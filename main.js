cebbeda92792981321df1b11ebc387d7dbc1a435
Joe <lolkiller1@hotmail.com>
2024-01-19 15:50:23 -0500

    autocommit

--- main.js
+++ main.js
@@ -1,8 +1,16 @@
 var util = require("util")
 var tess = require("tess")
 
+for(const i in Game.creeps) {
+    
+	if (Game.creeps[i].name == "tess") {
+		tess.run(Game.creeps[i])
+	}
+	
+	
+}
+
 util.spawnCreep("Tess", {m:2,w:1,c:1}, {status:"harvesting"}, 'E5N57')
 
-tess.run(Game.creeps["tess"])
 
 console.log("new test")
