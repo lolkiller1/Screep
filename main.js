a19e14b8d8ec28a19a5b9bb16220819a6fb70ff0
Joe <lolkiller1@hotmail.com>
2024-01-19 16:14:08 -0500

    autocommit

--- main.js
+++ main.js
@@ -6,9 +6,16 @@ for(const i in Memory.creeps) {
     if (Game.creeps[i] == undefined) {
         delete Memory.creeps[i]
     }
-    console.log(Game.creeps[i])
-	
+
 	if (Game.creeps[i].name == "tess") {
+		tess.run(Game.creeps[i])
+	}
+	
+	if (Game.creeps[i].name == "tess2") {
+		tess.run(Game.creeps[i])
+	}
+	
+	if (Game.creeps[i].name == "tess3") {
 		tess.run(Game.creeps[i])
 	}
 	
@@ -18,7 +25,8 @@ for(const i in Memory.creeps) {
 	
 }
 
-//util.spawnCreep("tess", {m:2,w:1,c:1}, {status:"harvesting"}, 'E5N57')
-
+util.spawnCreep("tess", {m:2,w:1,c:1}, {status:"harvesting"}, 'E5N57')
+util.spawnCreep("tess2", {m:2,w:1,c:1}, {status:"harvesting"}, 'E5N57')
+util.spawnCreep("tess3", {m:2,w:1,c:1}, {status:"harvesting"}, 'E5N57')
 
 console.log("new test")
