6dab0f980c8de0b627fb380ee89f6a693aac1f70
Joe <lolkiller1@hotmail.com>
2024-01-19 15:51:57 -0500

    autocommit

--- tess.js
+++ tess.js
@@ -10,6 +10,8 @@ module.exports = {
 			tess.memory.status = "harvesting"
 		}
 		
+		console.log(storageStatus)
+		
 		if (tess.memory.status == "harvesting") {
 			util.gatherFromEnergy(tess)
 		} else {
