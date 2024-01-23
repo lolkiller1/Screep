e6b10bb8e0d619dc689d99a543334977aa26a2c5
Joe <lolkiller1@hotmail.com>
2024-01-23 13:02:19 -0500

    autocommit

--- tower.js
+++ tower.js
@@ -7,7 +7,7 @@ module.exports = {
 		if (enemy) {
 			tower.attack(enemy)
 		} else {
-			var roads = tower.room.find(FIND_MY_STRUCTURES, {filter: { structureType: STRUCTURE_ROAD }
+			var roads = tower.room.find(FIND_MY_STRUCTURES, {filter: { structureType: STRUCTURE_ROAD } })
 			roads = _.filter(roads, function(road){return road.hits * 2 < road.hitsMax})
 			if (roads[0]) {
 				tower.repair(road)
