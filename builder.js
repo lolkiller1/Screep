9e531f84eb3ffa259c8a292331453e822d0848c1
Joe <lolkiller1@hotmail.com>
2024-01-23 13:05:07 -0500

    autocommit

--- builder.js
+++ builder.js
@@ -11,7 +11,7 @@ module.exports = {
 		}
 		
 		if (creep.memory.status == "harvesting") {
-			util.gatherFromEnergy(creep)
+			util.gatherFromSpot(name, 31, 7)
 		} else {
 			var spot = util.findClosestBuildable(creep)
 			if (creep.pos.getRangeTo(spot) > 3) {
