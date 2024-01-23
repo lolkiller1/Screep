077b0cf303b89206e7ed1fdf3b680353d84018d2
Joe <lolkiller1@hotmail.com>
2024-01-23 12:59:58 -0500

    autocommit

--- tower.js
+++ tower.js
@@ -1,10 +1,19 @@
 var util = require("util")
+var _ = require("lodash")
 
 module.exports = {
 	run(tower){
 		var enemy = tower.pos.findClosestByRange(FIND_HOSTILE_CREEPS)
 		if (enemy) {
 			tower.attack(enemy)
+		} else {
+			const roads = tower.room.find(FIND_MY_STRUCTURES, {filter: { structureType: STRUCTURE_ROAD }
+			roads = _.filter(roads, function(road){return road.hits * 2 < road.hitsMax})
+			if (roads[0]) {
+				tower.repair(road)
+			}
+			
+});
 		}
 	}
 	
