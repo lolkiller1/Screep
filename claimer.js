92519eed846513f6b839bbab1eb846ab291bc37d
Joe <lolkiller1@hotmail.com>
2024-01-24 15:35:00 -0500

    autocommit

--- claimer.js
+++ claimer.js
@@ -2,7 +2,7 @@ module.exports = {
 	run(creep){
 		
 		if (creep.room.name == "E5N57") creep.moveTo(new RoomPosition(0, 14, 'E5N57'))
-		if (creep.room.name == "E4N57") console.log(creep.moveTo(new RoomPosition(43, 0, "E4N58")))
+		if (creep.room.name == "E4N57") console.log(creep.moveTo(new RoomPosition(43, 0, "E4N57")))
 		if (creep.room.name == "E4N58") creep.moveTo(new RoomPosition(25, 20, "E3N58"))
 		if (creep.room.name == "E3N58") creep.moveTo(new RoomPosition(25, 20, "E3N59"))
 		if (creep.room.name == "E3N59") creep.moveTo(new RoomPosition(25, 20, "E3N60"))
