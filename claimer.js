e6ecd20df3c148c2199b8df7c0e300b3ff4c4fbc
Joe <lolkiller1@hotmail.com>
2024-01-24 15:36:47 -0500

    autocommit

--- claimer.js
+++ claimer.js
@@ -3,13 +3,13 @@ module.exports = {
 		
 		if (creep.room.name == "E5N57") creep.moveTo(new RoomPosition(0, 14, 'E5N57'))
 		if (creep.room.name == "E4N57") console.log(creep.moveTo(new RoomPosition(43, 0, "E4N57")))
-		if (creep.room.name == "E4N58") creep.moveTo(new RoomPosition(25, 20, "E3N58"))
-		if (creep.room.name == "E3N58") creep.moveTo(new RoomPosition(25, 20, "E3N59"))
-		if (creep.room.name == "E3N59") creep.moveTo(new RoomPosition(25, 20, "E3N60"))
-		if (creep.room.name == "E3N60") creep.moveTo(new RoomPosition(25, 20, "E4N60"))
-		if (creep.room.name == "E4N60") creep.moveTo(new RoomPosition(25, 20, "E5N60"))
-		if (creep.room.name == "E5N60") creep.moveTo(new RoomPosition(25, 20, "E6N60"))
-		if (creep.room.name == "E6N60") creep.moveTo(new RoomPosition(25, 20, "E6N59"))
+		if (creep.room.name == "E4N58") creep.moveTo(new RoomPosition(0, 6, "E4N58"))
+		if (creep.room.name == "E3N59") creep.moveTo(new RoomPosition(25, 20, "E3N59"))
+		if (creep.room.name == "E3N60") creep.moveTo(new RoomPosition(25, 20, "E3N60"))
+		if (creep.room.name == "E4N60") creep.moveTo(new RoomPosition(25, 20, "E4N60"))
+		if (creep.room.name == "E5N60") creep.moveTo(new RoomPosition(25, 20, "E5N60"))
+		if (creep.room.name == "E6N60") creep.moveTo(new RoomPosition(25, 20, "E6N60"))
+		if (creep.room.name == "E6N59") creep.moveTo(new RoomPosition(25, 20, "E6N59"))
 			
 		if (creep.room.name == "E6N59") {
 			var ob = Game.room.E6N59.controller
