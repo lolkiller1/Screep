fdd65608398e8afc256092ed5b49dfa9c0d29be2
Joe <lolkiller1@hotmail.com>
2024-01-24 22:23:43 -0500

    autocommit

--- main.js
+++ main.js
@@ -124,9 +124,9 @@ for(const i in Memory.creeps) {
 }
 
 
-util.spawnCreep("outUpgrader",  {m:6,w:3,c:3}, {status:"harvesting"}, 'E5N57')
-util.spawnCreep("outUpgrader2",  {m:6,w:3,c:3}, {status:"harvesting"}, 'E5N57')
-util.spawnCreep("outUpgrader3",  {m:6,w:3,c:3}, {status:"harvesting"}, 'E5N57')
+util.spawnCreep("outUpgrader",  {m:2,w:1,c:1}, {status:"harvesting"}, 'E5N57')
+util.spawnCreep("outUpgrader2",  {m:2,w:1,c:1}, {status:"harvesting"}, 'E5N57')
+util.spawnCreep("outUpgrader3",  {m:2,w:1,c:1}, {status:"harvesting"}, 'E5N57')
 util.spawnCreep("outUpgraderB",  {m:6,w:3,c:3}, {status:"harvesting"}, 'E5N57')
 util.spawnCreep("outUpgrader2B",  {m:6,w:3,c:3}, {status:"harvesting"}, 'E5N57')
 util.spawnCreep("outUpgrader3B",  {m:6,w:3,c:3}, {status:"harvesting"}, 'E5N57')
