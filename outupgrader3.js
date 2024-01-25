7e1071bec4c95169c203ccd5362a0dd24a45e810
Joe <lolkiller1@hotmail.com>
2024-01-24 22:44:21 -0500

    autocommit

--- outupgrader3.js
+++ outupgrader3.js
@@ -42,7 +42,7 @@ module.exports = {
 	},
 	build(creep){
 		
-		if (creep.room.name != "E6N59") {
+		if (creep.room.name != "E6N57") {
 			if (creep.room.name == "E5N57") creep.moveTo(new RoomPosition(0, 14, 'E5N57'))
 			if (creep.room.name == "E4N57") creep.moveTo(new RoomPosition(43, 0, "E4N57"))
 			if (creep.room.name == "E4N58") creep.moveTo(new RoomPosition(0, 6, "E4N58"))
