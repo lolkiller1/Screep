c74dbee4207439ec1e819de55e03d80e68720e06
Joe <lolkiller1@hotmail.com>
2024-01-27 15:20:14 -0500

    autocommit

--- roomUpgrader.js
+++ roomUpgrader.js
@@ -14,11 +14,20 @@ module.exports = {
 			util.gatherFromEnergy(creep)
 		} else {
 			var spawn = Game.spawns[creep.room.name]
+			var extensions = creep.room.find(FIND_MY_STRUCTURES, {filter: { structureType: STRUCTURE_EXTENSION }})
+			extensions = _.filter(extensions, function(extension) {return extension.store.getFreeCapacity(RESOURCE_ENERGY) > 0})
+			spot = creep.pos.findClosestByPath(extensions)
 			if (spawn.store.getFreeCapacity(RESOURCE_ENERGY) > 0) {
 				if (creep.pos.getRangeTo(spawn) > 1) {
 					creep.moveTo(spawn)
 				} else {
 					creep.transfer(spawn,RESOURCE_ENERGY)
+				}
+			} else if (spot) {
+				if (creep.pos.getRangeTo(spot) > 1) {
+					creep.moveTo(spot)
+				} else {
+					creep.transfer(spot, RESOURCE_ENERGY)
 				}
 			} else {
 				if (creep.pos.getRangeTo(creep.room.controller) > 3) {
