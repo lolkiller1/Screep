d2b446ba3f801db44693c3b3a814205cdf6a6b4e
Joe <lolkiller1@hotmail.com>
2024-01-31 17:36:28 -0500

    autocommit

--- room.js
+++ room.js
@@ -20,21 +20,21 @@ module.exports = {
 				util.spawnCreep(roomName + "_Miner", room.energyCapacityAvailable >= 750 ? {w:5,m:5} : room.energyCapacityAvailable >= 500 ? {m:2,w:4} : {m:1,w:2},{status:"harvesting"},roomName)
 			}
 			
-			var upgrader = Game.creeps[roomName + "_Upgrader"]
-			if (upgrader) {
-				roomUpgrader.run(upgrader)
-			} else {
-				util.spawnCreep(roomName + "_Upgrader", room.energyAvailable >= 750 ? {c:3,m:6,w:3} : room.energyAvailable >= 500 ? {m:4,c:2,w:2} : {m:2,c:1,w:1},{status:"harvesting"},roomName)
-			}
-			
 			var constructionSpots = room.find(FIND_MY_CONSTRUCTION_SITES)
 			if (constructionSpots[0]) {
 				var builder = Game.creeps[roomName + "_Builder"]
 				if (builder) {
 					roomUpgrader.build(builder)
 				} else {
-					util.spawnCreep(roomName + "_Builder",room.energyAvailable >= 500 ? {m:4,c:3,w:1} : {m:2,c:1,w:1},{status:"harvesting"},roomName)
+					util.spawnCreep(roomName + "_Builder",room.energyCapacityAvalable >= 750 ? {w:2,c:4,m:6} : room.energyCapacityAvalable >= 500 ? {m:4,c:3,w:1} : {m:2,c:1,w:1},{status:"harvesting"},roomName)
 				}
+			}
+			
+			var upgrader = Game.creeps[roomName + "_Upgrader"]
+			if (upgrader) {
+				roomUpgrader.run(upgrader)
+			} else {
+				util.spawnCreep(roomName + "_Upgrader", room.energyAvailable >= 750 ? {c:3,m:6,w:3} : room.energyAvailable >= 500 ? {m:4,c:2,w:2} : {m:2,c:1,w:1},{status:"harvesting"},roomName)
 			}
 		}
 	}
