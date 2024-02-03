7d579508e21629a58da091780998df17cf9a5f92
Joe <lolkiller1@hotmail.com>
2024-02-02 22:35:40 -0500

    autocommit

--- main.js
+++ main.js
@@ -158,6 +158,7 @@ util.spawnCreep("outUpgrader2B",  {m:10,w:7,c:3}, {status:"harvesting"}, 'E5N57'
 util.spawnCreep("outUpgrader3B",  {m:6,w:3,c:3}, {status:"harvesting"}, 'E5N57')
 */
 
+util.spawnCreep("dangerGather", {c:15,m:15}, {status:"harvesting"},'E5N57')
 util.spawnCreep("dangerMiner", {w:7,m:7}, {},'E5N57')
 util.spawnCreep("tessTheKiller",  {r:4,m:8,h:4}, {type:"ranged"}, 'E5N57')
 util.spawnCreep("tessTheKiller2",  {r:4,m:8,h:4}, {type:"ranged"}, 'E5N57')
