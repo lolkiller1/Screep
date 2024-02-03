aa80412cc8bc68391656520e5f11417b5d30cb72
Joe <lolkiller1@hotmail.com>
2024-02-02 21:47:02 -0500

    autocommit

--- roomUpgrader.js
+++ roomUpgrader.js
@@ -60,13 +60,20 @@ module.exports = {
 				util.gatherFromEnergy(creep)
 			}
 		} else {
-			//var towers = creep.room.find(FIND_MY_STRUCTURES, {filter: function(object) {return object.structureType == STRUCTURE_TOWER && object && object.store && object.store.getFreeCapacity(RESOURCE_ENERGY) > 0}})
-			//var tower = creep.pos.findClosestByPath(tower)
+			var towers = creep.room.find(FIND_MY_STRUCTURES, {filter: function(object) {
+				if (object) {
+					if (object.structureType == STRUCTURE_TOWER) {
+						return object.store.getFreeCapacity(RESOURCE_ENERGY) > 0
+					}
+				}
+				return false
+			}})
+			var tower = creep.pos.findClosestByPath(tower)
 			
 			var extensions = creep.room.find(FIND_MY_STRUCTURES, {filter: { structureType: STRUCTURE_EXTENSION }})
 			extensions = _.filter(extensions, function(extension) {return extension.store.getFreeCapacity(RESOURCE_ENERGY) > 0})
 			spot = creep.pos.findClosestByPath(extensions)
-			if (false) {
+			if (tower.store.getFreeCapacity(RESOURCE_ENERGY) > 0) {
 				if (creep.pos.getRangeTo(tower) > 1) {
 					creep.moveTo(tower)
 				} else {
