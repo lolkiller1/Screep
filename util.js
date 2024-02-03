5b799b48f154f6c87334b1d063cba4bf1e9dc85f
Joe <lolkiller1@hotmail.com>
2024-02-02 19:16:51 -0500

    autocommit

--- util.js
+++ util.js
@@ -87,13 +87,13 @@ module.exports = {
 	},
 	getOppositeDirection(direction) {
 		switch(direction) {
-			case:TOP return BOTTOM
-			case:TOP_RIGHT return BOTTOM_LEFT
-			case:RIGHT return LEFT
-			case:BOTTOM_RIGHT return TOP_LEFT
-			case:BOTTOM return TOP 
-			case:BOTTOM_LEFT return TOP_RIGHT 
-			case:LEFT return RIGHT
+			case TOP: return BOTTOM
+			case TOP_RIGHT: return BOTTOM_LEFT
+			case RIGHT: return LEFT
+			case BOTTOM_RIGHT: return TOP_LEFT
+			case BOTTOM: return TOP 
+			case BOTTOM_LEFT: return TOP_RIGHT 
+			case LEFT: return RIGHT
 			default: return BOTTOM_RIGHT
 		}
 	}
