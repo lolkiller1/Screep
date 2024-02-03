8ae7a963672110d735604f017cee833f5be3d4cb
Joe <lolkiller1@hotmail.com>
2024-02-02 22:56:24 -0500

    autocommit

--- name.js
+++ name.js
@@ -5,26 +5,22 @@ module.exports = {
 	run(name){
 		util.setStatus(name)
 		
-		if (name.memory.status == "harvesting") {
+		var spot = Game.spawns["E5N57"]
+		var extensions = name.room.find(FIND_MY_STRUCTURES, {filter: { structureType: STRUCTURE_EXTENSION }})
+		extensions = _.filter(extensions, function(extension) {return extension.store.getFreeCapacity(RESOURCE_ENERGY) > 0})
+		spot = spot ? spot : name.pos.findClosestByPath(extensions)
+		
+		
+		if (name.memory.status == "harvesting" || !spot) {
 			util.gatherFromStorage(name)
 		} else {
-			var spot = Game.spawns["E5N57"]
 			if (spot.store.getFreeCapacity(RESOURCE_ENERGY) > 0) {
 				if (name.pos.getRangeTo(spot) > 1) {
 					name.moveTo(spot)
 				} else {
 					name.transfer(spot, RESOURCE_ENERGY)
 				}
-			} else {
-				var extensions = name.room.find(FIND_MY_STRUCTURES, {filter: { structureType: STRUCTURE_EXTENSION }})
-				extensions = _.filter(extensions, function(extension) {return extension.store.getFreeCapacity(RESOURCE_ENERGY) > 0})
-				spot = name.pos.findClosestByPath(extensions)
-				
-				if (!spot) {
-					creep.memory.status = "harvesting"
-					return
-				}
-				
+			} else {				
 				if (name.pos.getRangeTo(spot) > 1) {
 					name.moveTo(spot)
 				} else {
