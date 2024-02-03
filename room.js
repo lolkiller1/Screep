e16f4e5150951302acc5e22da00538e61fac2729
Joe <lolkiller1@hotmail.com>
2024-02-03 17:55:45 -0500

    autocommit

--- room.js
+++ room.js
@@ -49,7 +49,7 @@ module.exports = {
 		var upgrader2 = Game.creeps[roomName + "_Upgrader2"]
 		if (upgrader2) {
 			roomUpgrader.run2(upgrader2, towers)
-		} else if (upgrader && upgrader.hits > 500) {
+		} else if (upgrader && upgrader.hits > 500 && miner && miner.hits > 500) {
 			util.spawnCreep(roomName + "_Upgrader2", room.energyAvailable >= 1300 ? {w:5,c:5,m:10} : room.energyAvailable >= 750 ? {c:3,m:6,w:3} : room.energyAvailable >= 500 ? {m:4,c:2,w:2} : {m:2,c:1,w:1},{status:"harvesting"},roomName)
 		}
 	}
