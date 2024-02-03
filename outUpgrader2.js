143e4988633a1a15d68cbbe47e8b2dd4a4fca8ec
Joe <lolkiller1@hotmail.com>
2024-02-02 23:44:15 -0500

    autocommit

--- outUpgrader2.js
+++ outUpgrader2.js
@@ -3,21 +3,10 @@ var util = require("util")
 module.exports = {
 	run(creep){
 		
-		if (creep.room.name != "E7N57"){
-		if (creep.room.name == "E5N57") creep.moveTo(new RoomPosition(0, 14, 'E5N57'))
-		if (creep.room.name == "E4N57") creep.moveTo(new RoomPosition(43, 0, "E4N57"))
-		if (creep.room.name == "E4N58") creep.moveTo(new RoomPosition(0, 6, "E4N58"))
-		if (creep.room.name == "E3N58") creep.moveTo(new RoomPosition(27, 0, "E3N58"))
-		if (creep.room.name == "E3N59") creep.moveTo(new RoomPosition(36, 0, "E3N59"))
-		if (creep.room.name == "E3N60") creep.moveTo(new RoomPosition(49, 40, "E3N60"))
-		if (creep.room.name == "E4N60") creep.moveTo(new RoomPosition(49, 38, "E4N60"))
-		if (creep.room.name == "E5N60") creep.moveTo(new RoomPosition(49, 30, "E5N60"))
-		if (creep.room.name == "E6N60") creep.moveTo(new RoomPosition(20, 49, "E6N60"))
-		if (creep.room.name == "E6N59") creep.moveTo(new RoomPosition(36, 49, "E6N59"))
-		if (creep.room.name == "E6N58") creep.moveTo(new RoomPosition(49, 18, "E6N58"))
-		if (creep.room.name == "E7N58") creep.moveTo(new RoomPosition(28, 49, "E7N58"))
-		return
+		if (creep.room.name != "E7N57") {
+			creep.moveTo(new RoomPosition(50,50,"E7N57"))
 		}
+		
 			
 		
 		util.setStatus(creep)
@@ -48,21 +37,7 @@ module.exports = {
 	},
 	build(creep){
 		
-		if (creep.room.name != "E7N57"){
-		if (creep.room.name == "E5N57") creep.moveTo(new RoomPosition(0, 14, 'E5N57'))
-		if (creep.room.name == "E4N57") creep.moveTo(new RoomPosition(43, 0, "E4N57"))
-		if (creep.room.name == "E4N58") creep.moveTo(new RoomPosition(0, 6, "E4N58"))
-		if (creep.room.name == "E3N58") creep.moveTo(new RoomPosition(27, 0, "E3N58"))
-		if (creep.room.name == "E3N59") creep.moveTo(new RoomPosition(36, 0, "E3N59"))
-		if (creep.room.name == "E3N60") creep.moveTo(new RoomPosition(49, 40, "E3N60"))
-		if (creep.room.name == "E4N60") creep.moveTo(new RoomPosition(49, 38, "E4N60"))
-		if (creep.room.name == "E5N60") creep.moveTo(new RoomPosition(49, 30, "E5N60"))
-		if (creep.room.name == "E6N60") creep.moveTo(new RoomPosition(20, 49, "E6N60"))
-		if (creep.room.name == "E6N59") creep.moveTo(new RoomPosition(36, 49, "E6N59"))
-		if (creep.room.name == "E6N58") creep.moveTo(new RoomPosition(49, 18, "E6N58"))
-		if (creep.room.name == "E7N58") creep.moveTo(new RoomPosition(28, 49, "E7N58"))
-		return
-		}
+		
 		
 		util.setStatus(creep)
 		
