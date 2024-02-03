7334f86a1d4a86f0761aa0f9525b56c80cffcb23
Joe <lolkiller1@hotmail.com>
2024-02-02 22:10:21 -0500

    autocommit

--- dangerMiner.js
+++ dangerMiner.js
@@ -3,23 +3,25 @@ var util = require("util")
 
 module.exports = {
 	run(creep){
-		util.setStatus(creep)
-		
-		if (creep.memory.status == "harvesting") {
-			if (creep.room.name == "E5N57") {
-				creep.moveTo(20,49)
-				return
-			}
-		} else {
-			if (creep.room.name == "E5N57") {
-				creep.moveTo(20,49)
-				return
-			}
-		}
-		
 		if (creep.room.name == "E5N57") {
 			creep.moveTo(20,49)
 			return
 		}
+		
+		var source = creep.pos.findClosestByPath(FIND_SOURCES)
+		var enemy = creep.pos.findClosestByPath(FIND_HOSTILE_CREEPS)
+		
+		if (creep.pos.getRangeTo(enemy) < 4) {
+			var direction = creep.pos.getDirectionTo(enemy)
+			creep.move(util.getOppositeDirection(direction))
+			return
+		}
+		
+		if (creep.getRangeTo(source) > 1) {
+			creep.moveTo(source)
+		} else {
+			creep.harvest(source)
+		}
+		
 	}
 }
