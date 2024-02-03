bcdb55a5e4c5d29ffd1b21122bd32e2a09e017d3
Joe <lolkiller1@hotmail.com>
2024-02-02 21:38:20 -0500

    autocommit

--- roomUpgrader.js
+++ roomUpgrader.js
@@ -44,6 +44,51 @@ module.exports = {
 			}
 		}
 	},
+	run2(creep){
+		var storageStatus = util.getCreepStorageStatus(creep)
+		util.setStatus(creep)
+		
+		if (creep.memory.status == "harvesting") {
+			var drop = util.findClosestDrop(creep)
+			if (drop) {
+				if (creep.pos.getRangeTo(drop) > 1){
+					creep.moveTo(drop)
+				} else {
+					creep.pickup(drop)
+				}
+			} else {
+				util.gatherFromEnergy(creep)
+			}
+		} else {
+			var towers = room.find(FIND_MY_STRUCTURES, {filter: function(object) {return object.structureType == STRUCTURE_TOWER && structure.store.getFreeCapacity(RESOURCE_ENERGY) > 0}})
+			
+			
+			var extensions = creep.room.find(FIND_MY_STRUCTURES, {filter: { structureType: STRUCTURE_EXTENSION }})
+			extensions = _.filter(extensions, function(extension) {return extension.store.getFreeCapacity(RESOURCE_ENERGY) > 0})
+			spot = creep.pos.findClosestByPath(extensions)
+			if (tower.store.getFreeCapacity(RESOURCE_ENERGY) > 0) {
+				if (creep.pos.getRangeTo(tower) > 1) {
+					creep.moveTo(tower)
+				} else {
+					creep.transfer(tower,RESOURCE_ENERGY)
+				}
+			} else if (spot) {
+				if (creep.pos.getRangeTo(spot) > 1) {
+					creep.moveTo(spot)
+				} else {
+					creep.transfer(spot, RESOURCE_ENERGY)
+					extensions = _.filter(extensions, function(e) {return e.id != spot.id})
+					creep.moveTo(creep.pos.findClosestByPath(extensions))
+				}
+			} else {
+				if (creep.pos.getRangeTo(creep.room.controller) > 3) {
+					creep.moveTo(creep.room.controller)
+				} else {
+					creep.upgradeController(creep.room.controller)
+				}
+			}
+		}
+	},
 	build(creep){
 		var storageStatus = util.getCreepStorageStatus(creep)
 		util.setStatus(creep)
