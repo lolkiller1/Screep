d282b54c071de74d7cbaafff2e13faffeb87145b
Joe <lolkiller1@hotmail.com>
2024-02-03 18:14:19 -0500

    autocommit

--- roomTask.js
+++ roomTask.js
@@ -27,7 +27,7 @@ module.exports = {
 	},
 	getExtensions(room) {
 		var extensions
-		if (Game.time % 10 == 0 && Memory[room.name].extensions) {
+		if (Game.time % 10 == 0 && Memory[room.name].extensions != null) {
 			extensions = []
 			for (var e in Memory[room.name].extensions) {
 				extensions.push(Game.getObjectById(Memory[room.name].extensions[e]))
