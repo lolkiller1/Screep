1fd6b57f92062eacc01dce263e714c706a142753
Joe <lolkiller1@hotmail.com>
2024-02-04 01:10:13 -0500

    autocommit

--- main.js
+++ main.js
@@ -246,12 +246,12 @@ if (!Game.creeps["waller"] && !spawning) {
 	---------
 	OUTCREEPS
 	---------
-*/
+
 
 if (!Game.creeps["claimer"]) {
 	util.spawnCreep("claimer", {claim:1,m:1}, {status:"harvesting"},'E7N58')
 }
-
+*/
 
 if (!Game.creeps["outUpgrader"]) {
 	util.spawnCreep("outUpgrader", {w:3,c:3,m:6}, {status:"harvesting"},'E7N57')
