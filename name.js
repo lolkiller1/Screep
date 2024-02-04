3d22fa2a11376250641861ecb40b257c92d5634d
Joe <lolkiller1@hotmail.com>
2024-02-04 01:38:44 -0500

    autocommit

--- name.js
+++ name.js
@@ -5,37 +5,24 @@ module.exports = {
 	run(name){
 		util.setStatus(name)
 		
+		var spot = Game.spawns["E5N57"]
+		var extensions = name.room.find(FIND_MY_STRUCTURES, {filter: { structureType: STRUCTURE_EXTENSION }})
+		extensions = _.filter(extensions, function(extension) {return extension.store.getFreeCapacity(RESOURCE_ENERGY) > 0})
+		spot = spot.store.getFreeCapacity(RESOURCE_ENERGY) > 0 ? spot : name.pos.findClosestByPath(extensions)
+		
+		if (!spot) {
+			filler.run(name)
+			return
+		}
+		
 		if (name.memory.status == "harvesting") {
 			util.gatherFromStorage(name)
 		} else {
-			var spot = Game.spawns["E5N57"]
-			if (spot.store.getFreeCapacity(RESOURCE_ENERGY) > 0) {
-				if (name.pos.getRangeTo(spot) > 1) {
-					name.moveTo(spot)
-				} else {
-					name.transfer(spot, RESOURCE_ENERGY)
-				}
+			if (name.pos.getRangeTo(spot) > 1) {
+				name.moveTo(spot)
 			} else {
-				var extensions = name.room.find(FIND_MY_STRUCTURES, {filter: { structureType: STRUCTURE_EXTENSION }})
-				extensions = _.filter(extensions, function(extension) {return extension.store.getFreeCapacity(RESOURCE_ENERGY) > 0})
-				spot = name.pos.findClosestByPath(extensions)
-				
-				if (!spot) {
-					filler.run(name)
-					return
-				}
-				
-				if (name.pos.getRangeTo(spot) > 1) {
-					name.moveTo(spot)
-				} else {
-					name.transfer(spot, RESOURCE_ENERGY)
-					extensions = _.filter(extensions, function(e) {return e.id != spot.id})
-					name.moveTo(name.pos.findClosestByPath(extensions))
-				}
-			}			
-			
-		}
-		
+				name.transfer(spot, RESOURCE_ENERGY)
+			}
+		}		
 	}
-	
 }
