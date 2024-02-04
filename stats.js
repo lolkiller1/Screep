c7616659a1f9b82c80b01a1a956aabb777852272
Joe <lolkiller1@hotmail.com>
2024-02-04 02:04:47 -0500

    autocommit

--- stats.js
+++ stats.js
@@ -1,6 +1,6 @@
 module.exports = {
 	roomLevels() {
-		var myRooms = ["E5N57","E6N57","E6N58","E7N57","E7N58","E8N59","E8N57"]
+		var myRooms = ["E5N57","E6N57","E6N58","E7N57","E7N58","E8N57","E8N59"]
 		for (var i in myRooms) {
 			var controller = Game.rooms[myRooms[i]].controller
 			console.log(Game.rooms[myRooms[i]] + ": " + controller.progress/controller.progressTotal)
