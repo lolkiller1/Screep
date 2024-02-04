eefc0e81fb64081bec2728851e1b3e3116bb9d27
Joe <lolkiller1@hotmail.com>
2024-02-04 00:21:22 -0500

    autocommit

--- stats.js
+++ stats.js
@@ -13,7 +13,7 @@ module.exports = {
 				var lowestAmt = 999999999
 				var hightest = 0
 				var highestAmt = -1 
-				var creep = Game.creeps[myRooms[w] + "_Miner"] ? Game.creeps[w + "_Miner"] : Game.creeps[w + "_Upgrader"]
+				var creep = Game.creeps[myRooms[w] + "_Miner"] != null ? Game.creeps[myRooms[w] + "_Miner"] : Game.creeps[myRooms[w] + "_Upgrader"]
 				var ramps = creep.room.find(FIND_STRUCTURES, {filter: {structureType:STRUCTURE_RAMPART}})
 				for (var i in ramps) {
 					var ramp = ramps[i]
