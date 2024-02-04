7d057f79ff52ff2209b6a8b257ac498b6758613a
Joe <lolkiller1@hotmail.com>
2024-02-03 19:19:32 -0500

    autocommit

--- tower.js
+++ tower.js
@@ -6,9 +6,9 @@ module.exports = {
 		var enemy = tower.pos.findClosestByRange(FIND_HOSTILE_CREEPS)
 		if (enemy) {
 			tower.attack(enemy)
-		} else {
+		} else if (Game.ticks % 10 == 0) {
 			var roads = tower.room.find(FIND_STRUCTURES, {filter: { structureType: STRUCTURE_RAMPART } })
-			roads = _.filter(roads, function(road){return road.hits < 50})
+			roads = _.filter(roads, function(road){return road.hits < 1000})
 			if (roads[0]) {
 				tower.repair(roads[0])
 			}
