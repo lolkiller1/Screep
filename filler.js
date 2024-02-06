2543bf3a748de7ea9aa211775e9c4cef8c454a00
Joe <lolkiller1@hotmail.com>
2024-02-06 13:51:06 -0500

    autocommit

--- filler.js
+++ filler.js
@@ -7,7 +7,7 @@ module.exports = {
 		if (creep.memory.status == "harvesting") {
 			var spot = creep.pos.findClosestByPath(FIND_DROPPED_RESOURCES, {filter: function(ob){return ob.resourceType == RESOURCE_ENERGY && ob.amount > 200}})
 			
-			if (!spot || creep.name.search("name") > -1) {
+			if (!spot || creep.name == "name" || creep.name == "name2") {
 				spot = Game.getObjectById("65bf22a6682254d4499b95d0")
 				if (creep.pos.getRangeTo(spot) > 1) {
 					creep.moveTo(spot)
