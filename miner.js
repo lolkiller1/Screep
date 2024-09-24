5fc3b2cadc4783f9587c2b73bac5e898d85ccd95
Joe <lolkiller1@hotmail.com>
2024-09-24 13:51:14 -0400

    autocommit

--- miner.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-	run(creep){
-		var source = creep.memory["source"]
-		
-		creep.moveTo(creep.memory.spot.x, creep.memory.spot.y)
-		creep.harvest(Game.getObjectById(source))
-	}
-}
